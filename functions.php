d6c6933583a58531bbf5de084363ed4110f69709
Wes1738 <wesleyrodriguessantos99@gmail.com>
2020-04-07 20:56:02 -0300

    Site - Produtos

--- functions.php
+++ functions.php
@@ -1,5 +1,16 @@
 <?php 
 
+use \Hcode\Model\User;
+
+function formatPrice($vlprice)
+{
+
+	if (!$vlprice > 0) $vlprice = 0;
+
+	return number_format($vlprice, 2, ",", ".");
+
+}
+
 function post($key)
 {
 	return str_replace("'", "", $_POST[$key]);
